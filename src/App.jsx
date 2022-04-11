df9281602e3f316381ce8f8b8a629c25f2ff1561
fnoopv <fnoopv@outlook.com>
2022-04-11 15:15:31 +0800

    feat: dns服务器支持多选;支持高级配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Form, Row, Col, Input, Button, Select, Empty, Table, message } from "antd";
+import {
+  Form,
+  Row,
+  Col,
+  Input,
+  Button,
+  Select,
+  Empty,
+  Table,
+  message,
+  InputNumber,
+} from "antd";
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./App.css";
@@ -9,15 +20,19 @@ const { Option } = Select;
 const App = () => {
   const [expand, setExpand] = useState(false);
   const [address, setAddress] = useState([]);
-  const [queryValue, setQueryValue] = useState({})
+  const [queryValue, setQueryValue] = useState({});
 
-  const reg = new RegExp('[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+')
+  const reg = new RegExp(
+    "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:.[a-zA-Z]{2,})+"
+  );
   const [form] = Form.useForm();
 
   const defaultQuery = {
     domain: "",
     type: "A",
     dns: ["114.114.114.114"],
+    timeout: 2,
+    tries: 3,
   };
 
   useEffect(() => {
@@ -41,8 +56,8 @@ const App = () => {
         </Form.Item>
       </Col>,
       <Col span={8} key="dns">
-        <Form.Item name="dns" label="DNS服务器">
-          <Select mode="multiple" allowClear placeholder="选择DNS服务器">
+        <Form.Item name="dns" label="DNS地址" tooltip="DNS服务地址,可以多选">
+          <Select mode="multiple" placeholder="选择DNS服务器">
             <Option key="114.114.114.114">114.114.114.114</Option>
             <Option key="119.29.29.29">119.29.29.29</Option>
             <Option key="223.5.5.5">223.5.5.5</Option>
@@ -53,6 +68,23 @@ const App = () => {
       </Col>,
     ];
 
+    const advanceChildren = [
+      <Col span={6} offset={4} key="timeout">
+        <Form.Item name="timeout" label="超时时间" tooltip="请求超时时间(s)">
+          <InputNumber min={1} max={60} />
+        </Form.Item>
+      </Col>,
+      <Col span={8} key="tries">
+        <Form.Item name="tries" label="重试次数" tooltip="失败重试次数">
+          <InputNumber min={1} max={5} />
+        </Form.Item>
+      </Col>,
+    ];
+
+    if (expand) {
+      children.push(...advanceChildren)
+    }
+
     return children;
   };
 
@@ -60,17 +92,21 @@ const App = () => {
     setQueryValue(values);
     const result = await query(values);
     if (result.msg.length > 0) {
-      message.error(result.msg, 3)
-      setAddress([])
+      message.error(result.msg, 3);
+      setAddress([]);
     } else if (result.data.length === 0) {
-      message.info("没有相关解析记录", 3)
-      setAddress([])
+      message.info("没有相关解析记录", 3);
+      setAddress([]);
     } else {
       for (const val of result.data) {
         if (!(val.result instanceof Array)) {
           if (reg.test(val.result)) {
-            const endResult = await query({ domain: val.result, type: "A", dns: queryValue.dns })
-            result.data.push(...endResult.data)
+            const endResult = await query({
+              domain: val.result,
+              type: "A",
+              dns: queryValue.dns,
+            });
+            result.data.push(...endResult.data);
           }
         }
       }
@@ -87,33 +123,41 @@ const App = () => {
     {
       title: "域名",
       key: "domain",
-      dataIndex: "domain"
+      dataIndex: "domain",
     },
     {
       title: "记录类型",
       key: "type",
-      dataIndex: "type"
+      dataIndex: "type",
     },
     {
       title: "记录值",
       key: "result",
-      dataIndex: "result"
+      dataIndex: "result",
     },
     {
       title: "操作",
       key: "action",
       render: (text, record) => (
-          <CopyToClipboard text={text.result}
-            onCopy={() => message.success(`${text.result} 已复制~`, 3)}>
-            <a>复制</a>
-          </CopyToClipboard>
+        <CopyToClipboard
+          text={text.result}
+          onCopy={() => message.success(`${text.result} 已复制~`, 3)}
+        >
+          <a>复制</a>
+        </CopyToClipboard>
       ),
-    }
-  ]
+    },
+  ];
 
   const resultList = () => {
     return (
-      <Table dataSource={address} columns={columns} rowKey={address.domain} pagination={false} size="small" />
+      <Table
+        dataSource={address}
+        columns={columns}
+        rowKey={address.domain}
+        pagination={false}
+        size="small"
+      />
     );
   };
 
@@ -125,8 +169,9 @@ const App = () => {
         className="ant-advanced-search-form"
         onFinish={queryDomain}
         initialValues={defaultQuery}
+        labelAlign="left"
       >
-        <Row gutter={24}>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
           <Col span={12} offset={6} key="domain">
             <div className="domainTitle">域名解析</div>
             <Form.Item
@@ -137,9 +182,10 @@ const App = () => {
                   message: "必须输入域名!",
                 },
                 {
-                  pattern: /[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/,
-                  message: "域名格式不正确"
-                }
+                  pattern:
+                    /[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/,
+                  message: "域名格式不正确",
+                },
               ]}
             >
               <Input placeholder="输入域名" size="large" autoFocus />
